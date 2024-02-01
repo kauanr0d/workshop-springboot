4505a2fe2a81f210a03bba6808716cbf3d711ef7
kauanr0d <kauankenney16752@gmail.com>
2024-02-01 20:33:47 -0300

    Service layer, component registration

--- src/main/java/com/kauanrodrigues/course/repositories/UserRepository.java
+++ src/main/java/com/kauanrodrigues/course/repositories/UserRepository.java
@@ -6,5 +6,4 @@ import com.kauanrodrigues.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-
 }
---
+++ src/main/java/com/kauanrodrigues/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.kauanrodrigues.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.kauanrodrigues.course.entities.User;
+import com.kauanrodrigues.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		return repository.findById(id).get();
+	}
+	
+}
