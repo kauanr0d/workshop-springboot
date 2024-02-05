03839bb6f8c9527e6d72c1c31cc2b20d574a57e0
kauanr0d <kauankenney16752@gmail.com>
2024-02-05 17:47:31 -0300

    Exception handling - delete

--- src/main/java/com/kauanrodrigues/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/kauanrodrigues/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.kauanrodrigues.course.services.exceptions.DatabaseException;
 import com.kauanrodrigues.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,12 +15,22 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	
-	@ExceptionHandler(ResourceNotFoundException.class)//o método irá interceptar qualquer execeção deste tipo que for lançada e realizar o tratamento
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class) // o método irá interceptar qualquer execeção deste tipo que for
+														// lançada e realizar o tratamento
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(), status.value(),error,e.getMessage(),request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+ 
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 }
--- src/main/java/com/kauanrodrigues/course/resources/exceptions/StandardError.java
+++ src/main/java/com/kauanrodrigues/course/resources/exceptions/StandardError.java
@@ -2,7 +2,10 @@ package com.kauanrodrigues.course.resources.exceptions;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError {
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
