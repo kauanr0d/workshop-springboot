428468ce368d327be48e9550328ab52fe956c7bc
kauanr0d <kauankenney16752@gmail.com>
2024-02-05 00:36:37 -0300

    User update

--- src/main/java/com/kauanrodrigues/course/services/UserService.java
+++ src/main/java/com/kauanrodrigues/course/services/UserService.java
@@ -34,4 +34,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id, User user) {
+		User entity = repository.getReferenceById(id);//objeto monitorado pelo JPA
+		
+		updateData(entity,user);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
+	
 }
