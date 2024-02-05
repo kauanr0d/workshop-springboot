ad9588c0917e61b844b77b02bdc5b8a1a28de20b
kauanr0d <kauankenney16752@gmail.com>
2024-02-05 00:27:46 -0300

    User delete

--- src/main/java/com/kauanrodrigues/course/resources/UserResource.java
+++ src/main/java/com/kauanrodrigues/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,19 +35,23 @@ public class UserResource {
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
+	public ResponseEntity<User> findById(@PathVariable Long id) {// path variable para indicar uma variável simples
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User user) {
+	public ResponseEntity<User> insert(@RequestBody User user) {// requestbody para passar o corpo do objeto
 		user = service.insert(user);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
-				buildAndExpand(user.getId()).toUri();
-		//padrao http: para retorno 201 a reponse tem que possuir um cabeçalho location 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		// padrao http: para retorno 201 a reponse tem que possuir um cabeçalho location
 		return ResponseEntity.created(uri).body(user);
 	}
-	
-	
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
